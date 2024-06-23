c6d6a56d4385e41cba364b49cdd1c59f932c5876
vaishnavighute <shubhampithe1111@gmail.com>
2024-06-23 13:18:34 +0530

    circular ll

--- CircularLinkedList/src/CircularLinkedList.java
+++ CircularLinkedList/src/CircularLinkedList.java
@@ -2,41 +2,121 @@
 public class CircularLinkedList {
 private Node head;
 
+
+
+public Node getHead() {
+	return head;
+}
 public CircularLinkedList() {
-head = null;
+	head=null;
+}
+public void setHead(Node head) {
+	this.head = head;
 }
 
+public CircularLinkedList(Node head) {
+	this.head = head;
+}
+
+//insert
 public boolean insert(int data) {
-	Node newNode=new Node(data);
-	if(newNode == null) {
-		return false;
+    Node newNode = new Node(data);
+    if (newNode == null) {
+        return false;
+    }
+
+    if (head == null) {
+        head = newNode;
+        head.setNext(head);
+        return true;
+    }
+
+    Node last = head;
+    while (last.getNext() != head) {
+        last = last.getNext();
+    }
+
+    last.setNext(newNode);
+    newNode.setNext(head);
+    return true;
+}
+	
+//display
+	public void display() {
+		if(head==null) {
+			return;
+		}
+		Node temp=head;
+		do {
+			System.out.println(temp.getData()+"");
+			temp=temp.getNext();
+		}while(temp!=head);
+		System.out.println();
 	}
 	
-	if(head == null) {
+	//insert at Last position
+	public boolean insertLast(int data) {
+		Node newNode=new Node(data);
+		Node last=head;
+		Node prev=head;
+		while(last.getNext()!=head) {
+			last=last.getNext();
+		}
+		newNode.setNext(prev);
+		last.setNext(newNode);
+		return true;
+	}
+	
+	//insert at first position
+	public boolean insertFirst(int data) {
+		Node newNode=new Node(data);
+		if(head == null) {
+			newNode.setNext(newNode);
+		}
+		else {
+			Node last=head;
+			while(last.getNext()!=head) {
+				last=last.getNext();
+			}
+			newNode.setNext(head);
+			last.setNext(newNode);
+		}
 		head=newNode;
-		head.setNext(head);
-  return true;
-}
-
-	Node last=head;
-	while(last.getNext()!=head) {
-		last=last.getNext();
+		return true;
 	}
-	last.setNext(newNode);
-	newNode.setNext(head);
-	return true;
-}
-
-public void display() {
-	if(head == null) {
-		return;
-	}
-	Node temp=head;
-	do {
-		System.out.println(temp.getData()+"");
-		temp=temp.getNext();
-	}while(temp!=head);
-	System.out.println();
 	
-}
+	//insert at specific position
+	public boolean insert(int data,int position) {
+		if((head==null && position>1)|| position<=0) {
+			return false;
+		}
+		Node newNode=new Node(data);
+		if(position == 1) {
+			if(head== null) {
+				head=newNode;
+				head.setNext(head);
+				return true;
+			}
+			Node last=head;
+			while(last.getNext()!=head) {
+				last=last.getNext();
+				
+			}
+			newNode.setNext(head);
+			head=newNode;
+			last.setNext(newNode);
+			return true;
+		}
+		Node prev=head;
+		for(int i=1;i<position-1;i++) {
+			prev=prev.getNext();
+			if(prev==head) {
+				return false;
+			}
+		}
+		newNode.setNext(prev.getNext());
+		prev.setNext(newNode);
+		return true;
+	}
+	
 }
--- CircularLinkedList/src/Main.java
+++ CircularLinkedList/src/Main.java
@@ -1,14 +1,22 @@
 
 public class Main {
-
-	public static void main(String[] args) {
-		CircularLinkedList l=new CircularLinkedList();
-l.insert(5);
-l.insert(60);
-l.insert(90);
-		l.display();
-		l.insert(4);
-		l.display();
-	}
-
+public static void main(String[] args) {
+CircularLinkedList q= new CircularLinkedList();	
+	
+	q.insert(5);
+	q.insert(90);
+	q.insert(6);	
+	q.insert(6);
+	System.out.println("elemets are: ");
+	q.display();
+	q.insertLast(1);
+	System.out.println("After adding node at last position: ");
+	q.display();
+	q.insertFirst(100);
+	System.out.println("After adding node at first position: ");
+	q.display();
+	q.insert(30, 3);
+	System.out.println("after adding element at position 3");
+	q.display();
+}
 }
