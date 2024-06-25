9b00c9ae44af568359a2c11f4857ea694400463f
vaishnavighute <shubhampithe1111@gmail.com>
2024-06-25 23:56:33 +0530

    DLL

--- DoubleLLMenuDriven/src/DoubleLinkedList.java
+++ DoubleLLMenuDriven/src/DoubleLinkedList.java
@@ -17,102 +17,68 @@ public void display() {
 		System.out.println(temp.getData()+"");
 		temp=temp.getNext();
 		
-	}
+}
 	System.out.println();
 }
-
-
-
-
-
 
 public void setHead(Node head) {
 	this.head = head;
 }
 
-//public boolean insert(int data) {
-//    Node newNode = new Node(data);
-//    if (newNode == null) {
-//        return false; 
-//    }
-//
-//    if (head == null) {
-//        head = newNode; 
-//        return true;
-//    }
-//
-//    Node last = head;
-//    while (last.getNext() != null) {
-//        last = last.getNext();
-//    }
-//
-//    last.setNext(newNode);
-//    newNode.setPrev(last);
-//
-//    return true;
-//}
-
-
-public boolean in(int data) {
-    Node newNode = new Node(data);
-    if (newNode == null) {
-        return false; 
-    }
-    if (head == null) {
-        head = newNode; 
-    }
-    Node last = head;
-    while (last.getNext() != null) {
-        last = last.getNext();
-    }
-
-    last.setNext(newNode);
-    newNode.setPrev(last);
-
-    return true;
+//insert by value
+public boolean insertByVal(int data) {
+	Node newNode=new Node(data) ;
+		if(newNode== null) {
+			return false;
+		}
+		if(head==null) {
+			head=newNode;
+			return true;
+		}
+		Node last=head;
+		while(last.getNext() !=null) {
+			last=last.getNext();
+			if(last.getData()==data) {
+				return false;
+			}
+		}
+		last.setNext(newNode);
+		newNode.setPrev(last);
+		return true;	
 }
 
-
-
-
-
-
-
-
-
-//insert ate perticular position
-
+//insert by position
 public boolean insert(int data, int position) {
-    if (position <= 0 || (head == null && position > 1)) {
-        return false; // Invalid position
+    if(position <= 0 || (head == null && position > 1)) {
+        return false;
     }
 
     Node newNode = new Node(data);
-    if (newNode == null) {
-        return false; // Memory allocation failed
+    if(newNode == null) {
+        return false;
     }
 
-    if (position == 1) {
-        if (head != null) {
+    if(position == 1) {
+        if(head != null) {
             newNode.setNext(head);
             head.setPrev(newNode);
         }
-        head = newNode; // New node becomes head
+        head = newNode;
         return true;
     }
 
     Node prevNode = head;
-    for (int i = 1; i < position - 1; i++) {
+    for(int i = 1; i < position - 1; i++) {
         prevNode = prevNode.getNext();
-        if (prevNode == null) {
-            return false; // Position exceeds list size  prev is set null
+        if(prevNode == null) {
+            return false;
         }
     }
 
     Node nextNode = prevNode.getNext();
 
     newNode.setPrev(prevNode);
-    if (nextNode != null) {
+    if(nextNode != null) {
         newNode.setNext(nextNode);
         nextNode.setPrev(newNode);
     }
@@ -121,17 +87,155 @@ public boolean insert(int data, int position) {
     return true;
 }
 
+//delete by val
+public boolean deleteByVal(int data) {
+    if (head == null) {
+        return false; // List is empty, nothing to delete
+    }
+    if (head.getData() == data) {
+        head = head.getNext(); // Move head to the next node
+        if (head != null) {
+            head.setPrev(null); 
+        }
+        return true;
+    }
+    Node del = head;
+    while (del != null && del.getData() != data) {
+        del = del.getNext(); 
+    }
+    if (del == null) {
+        return false; 
+    }
+   
+    Node prevNode = del.getPrev();
+    Node nextNode = del.getNext();
+    prevNode.setNext(nextNode); 
+    if (nextNode != null) {
+        nextNode.setPrev(prevNode); 
+    }
+    return true;
+}
 
+//delete by position
+public boolean deleteByPos(int pos) {
+	if(head == null || pos<=0) {
+		return false;
+	}
+	if(pos == 1) {
+		head=head.getNext();
+		if(head !=null) {
+			head.setPrev(null);
+		}
+		return true;
+	}
+	Node del=head;
+	for(int i=1 ;i<pos ;i++) {
+		del=del.getNext();
+		if(del == null) {
+			return false;
+		}
+		Node preNode= del.getPrev();
+		Node nextNode= del.getNext();
+	preNode.setNext(nextNode);
+	if(nextNode != null) {
+		nextNode.setPrev(preNode);
+	}
+	}
+	return true;
+}
 
+//count
+public int count() {
+    Node temp = head;
+    int count = 0;
+    while (temp != null) {
+        count++; 
+        temp = temp.getNext(); 
+    }
+    return count;
+}
 
+//remove duplicates
+public void removeDuplicates() {
+	if(head == null) {
+		return ;
+	}
+	Node c=head;
+	while(c!=null) {
+		Node r=c.getNext();
+		while(r!=null) {
+			if(r.getData() == c.getData()) {
+				Node nextNode=r.getNext();
+				if(nextNode !=null) {
+					nextNode.setPrev(r.getPrev());
+				}
+				r.getPrev().setNext(nextNode);
+				r=nextNode;
+			}
+			else {
+				r=r.getNext();
+			}
+		}
+		c=c.getNext();
+	}
+}
 
+//sort asc
+public void sortAsc() {
+	if(head == null || head.getNext() ==null) {
+		return ;
+	}
+	Node cur=head;
+	while(cur!=null) {
+	Node index=cur.getNext();
+		while(index !=null) {
+			if(cur.getData()> index.getData()) {
+				int temp=cur.getData();
+				cur.setData(index.getData());
+				index.setData(temp);
+			}
+			index=index.getNext();
+		}
+		cur=cur.getNext();
+	}
+}
 
+//desc sort
+public void sortDesc() {
+	if(head== null ||head.getNext() == null) {
+		return ;
+	}
+	Node cur=head;
+	while(cur!=null) {
+		Node index=cur.getNext();
+		while(index!=null) {
+			if(cur.getData() < index.getData()) {
+				int temp=cur.getData();
+				cur.setData(index.getData());
+				index.setData(temp);
+			}
+			index=index.getNext();
+		}
+		cur=cur.getNext();
+	}
+}
 
-
-
-
-
-
+//max element
+public void max() {
+	if(head == null) {
+		System.out.println("list is empty ");
+		return;
+	}
+	Node cur=head;
+	int maxVal=head.getData();
+	while(cur!=null) {
+		if(cur.getData() > maxVal) {
+			maxVal=cur.getData();
+		}
+		cur=cur.getNext();
+		System.out.println(maxVal);
+	}
+}
 
 
 }
--- DoubleLLMenuDriven/src/Main.java
+++ DoubleLLMenuDriven/src/Main.java
@@ -7,31 +7,60 @@ public class Main {
 		Scanner sc=new Scanner(System.in);
 		while(true) {
 			System.out.println("Enter choice:  1.insert data by position  2.insert data by val \n"
-					+ "3.delete by position   4.delete by value  ");
+					+ "3.delete by value   4.delete by position  5.count \n"
+					+ "6.remove duplicates  7.sorted in ascending order  8.sorted in descending order \n"
+					+ "9.max element   10.min element   11.search element ");
 			System.out.println("choice : ");
 			switch(sc.nextInt()) {
 			case 1:
-				//d.insert(1);
-				//d.insert(65);
-			//	System.out.println("added data using insert : ");
-			//d.display();
-			d.insert(10,1);	
-			d.insert(7, 2);
-			d.insert(8, 3);
-			d.insert(50, 4);
-			d.insert(5, 5);
-			System.out.println("data added using postion : ");
-			d.display();
-			d.in(0);
-			d.display();
-			
+				d.insertByVal(1);
+				d.insertByVal(3);
+				d.insertByVal(2);
+				d.insertByVal(3);
+				
+		System.out.println("Inserted data by value is : ");
+		d.display();
 				break;
-			
-			
-			
-			
-			
-			
+			case 2:
+				d.insert(10, 4);
+				d.insert(5, 5);
+				d.insert(90, 6);
+				System.out.println("Inserted data by position is : ");
+				d.display();
+			break;
+			case 3:
+				d.deleteByVal(10);
+				System.out.println("deleting value : ");
+				d.display();
+				break;
+			case 4:
+				d.deleteByPos(1);
+				System.out.println("element deleted by position : ");
+				d.display();
+				break;
+			case 5:
+				int count=d.count();
+				System.out.println("number of elements are: "+count);			
+				break;
+			case 6:
+				d.removeDuplicates();
+				System.out.println("successfully removed duplicates : ");
+				d.display();
+				break;
+			case 7:
+				d.sortAsc();
+				System.out.println("Elements in ascending order :");
+				d.display();
+				break;
+			case 8:
+				d.sortDesc();
+				System.out.println("Elements in descending order :"); 
+				d.display();
+				break;
+			case 9:
+				d.max();
+				
+				break;
 			}		
 		}
 
