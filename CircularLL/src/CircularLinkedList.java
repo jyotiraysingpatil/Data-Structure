b9ff83444fd0959c91289ee60b42bcfb44bc4f83
Jyoti Patil <jp893617@gmail.com>
2024-06-25 09:10:02 +0530

    CLL Menu driven code

---
+++ CircularLL/src/CircularLinkedList.java
@@ -0,0 +1,274 @@
+
+public class CircularLinkedList {
+	private Node head;
+
+	public CircularLinkedList() {
+		head=null;
+	}
+	public Node getHead() {
+		return head;
+	}
+	public void setHead(Node head) {
+		this.head = head;
+	}
+	public CircularLinkedList(Node head) {
+		this.head = head;
+	}
+
+//display
+	public void display() {
+		if(head == null) {
+			return;
+		}
+		Node temp=head;
+		do {
+			System.out.println(temp.getData()+"");
+			temp=temp.getNext();
+		}while(temp!=head);
+		System.out.println();
+	}
+
+	//insert by position
+	public boolean insertByPos(int data,int pos) {
+		Node newNode=new Node(data);
+		if(newNode==null) {
+			return false;
+		}
+		if(head==null) {
+			head=newNode;
+			head.setNext(head);
+			return true;
+		}
+		Node prev=head;
+		for(int i=1;i<pos-1;i++) {
+			prev=prev.getNext();
+		}
+		newNode.setNext(prev.getNext());
+		prev.setNext(newNode);
+		return true;
+	}
+	
+	//insert by val
+	public boolean insertByVal(int val) {
+	    Node newNode = new Node(val);
+	    if (newNode == null) {
+	        return false;
+	    }
+	    if (head == null || head.getData() >= newNode.getData()) {
+	        newNode.setNext(head);
+	        head = newNode;
+	        return true;
+	    }
+	    Node prev = head;
+	    while (prev.getNext() != null && prev.getNext().getData() < newNode.getData()) {
+	        prev = prev.getNext();
+	    }
+	    newNode.setNext(prev.getNext());
+	    prev.setNext(newNode);
+	    return true;
+	}
+
+	//delete by position
+	public boolean deleteByPos(int pos) {
+	    if (head == null || pos <= 0) {
+	        return false;
+	    }
+
+	    // Deleting the head node (position 1)
+	    if (pos == 1) {
+	        if (head.getNext() == head) {
+	            head = null; // Only one node in the list
+	            return true;
+	        }
+	        Node last = head;
+	        while (last.getNext() != head) {
+	            last = last.getNext();
+	        }
+	        head = head.getNext();
+	        last.setNext(head);
+	        return true;
+	    }
+
+	    Node prev = head;
+	    for (int i = 1; i < pos - 1; i++) {
+	        prev = prev.getNext();
+	        if (prev.getNext() == head) {
+	            return false; // Position is out of bounds
+	        }
+	    }
+	    Node del = prev.getNext();
+	    prev.setNext(del.getNext());
+	    return true;
+	}
+
+	//delete by value
+	public boolean deleteByVal(int data, int pos) {
+	    if (head == null || pos <= 0) {
+	        return false;
+	    }
+	    
+	    Node current = head;
+	    Node prev = null;
+
+	    if (pos == 1) {
+	        if (head.getNext() == head) {
+	            head = null;
+	            return true;
+	        }
+	        Node last = head;
+	        while (last.getNext() != head) {
+	            last = last.getNext();
+	        }
+
+	        last.setNext(head.getNext());
+	        head = head.getNext();
+	        return true;
+	    }
+	    for (int i = 1; i < pos - 1; i++) {
+	        current = current.getNext();
+	        if (current == head) {
+	            return false; 
+	        }
+	    }
+	    
+	    Node del = current.getNext();
+	    if (del == head) {
+	        return false; 
+	    }  
+	    current.setNext(del.getNext());
+	    return true;
+	}
+
+	//count of elements
+	public int count() {
+	    if (head == null) {
+	        return 0;
+	    }
+	    int count = 0;
+	    Node c = head;
+	    do {
+	        count++;
+	        c = c.getNext();
+	    } while (c != head);
+	    return count;
+	}
+
+	//remove duplicates
+	public void removeDuplicates() {
+		if(head == null) {
+			return ;
+		}
+		Node c=head;
+do {
+	Node r=c;
+	while(r.getNext()!=head) {
+		if(r.getNext().getData() == c.getData()) {
+			r.setNext(r.getNext().getNext());
+		}
+		else {
+			r=r.getNext();
+		}
+	}
+	c=c.getNext();
+}while(c!=head);
+}	
+
+	//sort in asc.
+	public void sort() {
+		if(head == null || head.getNext() == head) {
+			return;
+		}
+		Node cur=head;
+		Node index;
+		int temp;
+		do {
+			index=cur.getNext();
+			while(index!=head) {
+				if(cur.getData() >index.getData()) {
+					temp=cur.getData();
+					cur.setData(index.getData());
+					index.setData(temp);
+				
+				}
+				index=index.getNext();
+			}
+			cur=cur.getNext();
+			
+		}while(cur!=head);
+	}
+	
+	//sort list in desc.
+	public void sortDesc() {
+		if(head==null || head.getNext()== head) {
+			return;
+		}
+		Node cur=head;
+		Node index;
+		int temp;
+		do {
+			index=cur.getNext();
+			while(index!=head) {
+				if(cur.getData() < index.getData()) {
+					temp=cur.getData();
+					cur.setData(index.getData());
+					index.setData(temp);			
+				}
+				index=index.getNext();
+			}
+			cur=cur.getNext();
+		}while(cur !=head);
+	}
+	
+	//min ele.
+	public void min() {
+		if(head == null) {
+			System.out.println("list is empty ");
+			return;
+		}
+		Node cur=head;
+		int minval=head.getData();
+		do {
+			if(cur.getData() < minval) {
+				minval=cur.getData();
+			}
+			cur=cur.getNext();		
+		}while(cur !=head);
+		System.out.println(minval);
+	}
+	
+	//max ele.
+	public void max() {
+		if(head==null) {
+			System.out.println("list is empty ");
+			return;
+		}
+		Node cur=head;
+		int maxval=head.getData();
+		do {
+			if(cur.getData()> maxval) {
+				maxval=cur.getData();
+			}
+			cur=cur.getNext();
+		}while(cur!=head);
+		System.out.println(maxval);
+	}
+	
+	public boolean search(int key) {
+	    if (head == null) {
+	        return false; 
+	    }
+	    Node temp = head;
+
+	    do {
+	        if (temp.getData() == key) {
+	            return true; 
+	        }
+	        temp = temp.getNext();
+	    } while (temp != head);
+
+	    return false; 
+	}
+
+	
+	
+	}
---
+++ CircularLL/src/Main.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CircularLinkedList c=new CircularLinkedList();
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+			System.out.println("Enter choice: 1. insert by pos  2.insert by val   3.delete by pos "
+					+ " 4.delete by value   5.count of element  6.Remove Duplicates \n"
+					+ "7.sort list in ascending order  8.sort list in descending order \n "
+					+ "9.Minimum element from the list   10.Maximum element from the list  11.Search element");
+		System.out.println("choice : ");
+		switch(sc.nextInt()) {
+		case 1:
+		c.insertByPos(1, 1);
+		c.insertByPos(6, 3);
+		c.insertByPos(50, 2);
+		System.out.println("after adding element to specific position: ");
+		c.display();
+	break;
+		case 2:
+			c.insertByVal(50);
+			c.insertByVal(44);
+			System.out.println("after adding element by value : ");
+			c.display();
+	break;
+		case 3:
+			c.deleteByPos( 3);
+			System.out.println("after deleteting by pos : ");
+			c.display();
+	break;
+	case 4:
+		c.deleteByVal(60, 3);
+		System.out.println("after deleting by value: ");
+		c.display();
+		break;
+	case 5:
+		int count=c.count();
+		System.out.println(" total elements in list are : "+count);
+		break;
+	case 6:
+		c.removeDuplicates();
+		System.out.println("aftter removing duplicate elements from list: ");
+		c.display();
+		break;
+	case 7:
+		c.sort();
+		System.out.println("list sorted in ascending order :");
+		c.display();
+		break;
+	case 8:
+		c.sortDesc();
+		System.out.println("list sorted in descending order :");
+		c.display();
+		break;
+	case 9:
+		System.out.println("minimum element from list is : ");
+		c.min();
+	
+		break;
+	case 10:
+		System.out.println("maximum element from list is : ");
+		c.max();
+		break;
+	case 11:
+		System.out.println("eneter the value to be search: ");
+		int key=sc.nextInt();
+		System.out.println("enter the value to be searching  : "+ c.search(key));
+       
+		break;
+		}
+	}
+	}
+
+}
